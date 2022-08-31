034329716ff6f55550730bd6d2f021062cc65ae3
seongmin221 <seongmin221@naver.com>
2022-08-31 15:46:24 +0900

    [FEAT] snoring detection completed

--- SnoreClassifier/SnoreClassifier/Models/SoundResultsObserver.swift
+++ SnoreClassifier/SnoreClassifier/Models/SoundResultsObserver.swift
@@ -10,20 +10,25 @@ import SoundAnalysis
 
 class SoundResultsObserver: NSObject, SNResultsObserving {
     
+    var confidence: String = ""
+    
     func request(_ request: SNRequest, didProduce result: SNResult) {
         guard let result = result as? SNClassificationResult else { return }
         // TODO: classified results 중 가장 정확도 높은 것을 first로 가져옴
-        guard let classification = result.classifications.first else { return }
-        
-        // time of the capture inside the stream
-        let timeInSeconds = result.timeRange.start.seconds
-        let formattedTime = String(format: "%.2f", timeInSeconds)
-        print("Analysis result for audio at time: \(formattedTime)")
+        guard let snoringClassification = result.classification(forIdentifier: "1") else { return }
         
         // classification confidence
-        let confidence = classification.confidence * 100
-        let percentString = String(format: "%.2f%%", confidence)
-        print("Analysis result confidence: \(percentString)")
+        let snoringConfidence = snoringClassification.confidence * 100
+        if snoringConfidence >= 80 {
+            let snoringPercentString = String(format: "%.2f%%", snoringConfidence)
+            confidence = snoringPercentString
+            print("Snoring analysis result confidence: \(confidence)")
+            
+            // time of the capture inside the stream
+            let snoredTimeInSeconds = result.timeRange.start.seconds
+            let formattedTime = String(format: "%.2f", snoredTimeInSeconds)
+            print("Analysis result for audio at time: \(formattedTime)")
+        }
     }
     
     func request(_ request: SNRequest, didFailWithError error: Error) {
@@ -33,4 +38,5 @@ class SoundResultsObserver: NSObject, SNResultsObserving {
     func requestDidComplete(_ request: SNRequest) {
         print("Request completed!")
     }
+    
 }
---
+++ SnoreClassifier/SnoreClassifier/Screens/Views/FeedbackPopUpView.swift
@@ -0,0 +1,8 @@
+//
+//  FeedbackPopUpView.swift
+//  SnoreClassifier
+//
+//  Created by 이성민 on 2022/08/31.
+//
+
+import Foundation
